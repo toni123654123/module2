d35fa801306e9382bdc41296f3f68fb57c97c323
toni123654123 <tn1061999@gmail.com>
2020-09-15 17:15:41 +0700

    baitap

---
+++ src/week2/test/B.java
@@ -0,0 +1,11 @@
+package week2.test;
+
+public class B {
+    public static void main(String args[]){
+
+        A obj = new A();//Compile Time Error
+
+        obj.msg();//Compile Time Error
+
+    }
+}
