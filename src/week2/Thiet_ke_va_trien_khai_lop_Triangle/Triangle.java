94fc3ace626a3565dad2971be2b4d169e9adc3b8
toni123654123 <tn1061999@gmail.com>
2020-09-17 09:24:36 +0700

    Thiet_ke_va_trien_khai_lop_Triangle

---
+++ src/week2/Thiet_ke_va_trien_khai_lop_Triangle/Triangle.java
@@ -0,0 +1,9 @@
+package week2.Thiet_ke_va_trien_khai_lop_Triangle;
+
+public class Triangle {
+    public static void main(String[] args) {
+        Shape shape = new Shape(4,3,4);
+        System.out.println(shape.toString());
+
+    }
+}
