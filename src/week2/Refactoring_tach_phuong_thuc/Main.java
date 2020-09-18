a6d13070b588f42ea7a1b2d45acb6b52b502cbed
toni123654123 <tn1061999@gmail.com>
2020-09-18 16:50:24 +0700

    Refactoring

---
+++ src/week2/Refactoring_tach_phuong_thuc/Main.java
@@ -0,0 +1,8 @@
+package week2.Refactoring_tach_phuong_thuc;
+
+public class Main {
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder();
+        System.out.println(Cylinder.getVolume(3,4));
+    }
+}
