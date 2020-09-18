bccd580feb3963ca038422851538f0ca6a102460
toni123654123 <tn1061999@gmail.com>
2020-09-18 16:42:57 +0700

    Rèactoring

---
+++ src/week2/Refactoring_tach_bien/Main.java
@@ -0,0 +1,8 @@
+package week2.Refactoring_tach_bien;
+
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        System.out.println(FizzBuzz.fizzBuzz(6));
+    }
+}
