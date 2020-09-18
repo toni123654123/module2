f553924ab1ca21d89effa4c47eab64bb301df99e
toni123654123 <tn1061999@gmail.com>
2020-09-18 08:57:56 +0700

    Trien_khai_interface_Resizeable_cho_cac_lop_hinh_hocAssignment

---
+++ src/week2/Trien_khai_interface_Resizeable_cho_cac_lop_hinh_hocAssignment/Resizeable.java
@@ -0,0 +1,5 @@
+package week2.Trien_khai_interface_Resizeable_cho_cac_lop_hinh_hocAssignment;
+
+public interface Resizeable {
+    void resize(double percent);
+}
---
+++ src/week2/Trien_khai_interface_Resizeable_cho_cac_lop_hinh_hocAssignment/Square.java
@@ -0,0 +1,41 @@
+package week2.Trien_khai_interface_Resizeable_cho_cac_lop_hinh_hocAssignment;
+
+public class Square extends Rectangle {
+    public Square() {
+    }
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square(double side, String color, boolean filled) {
+        super(side, color, filled);
+    }
+
+    public double getSide() {
+        return getWidth();
+    }
+
+    public void setSide(double side) {
+        setWidth(side);
+        setLength(side);
+    }
+
+    @Override
+    public void setWidth(double width) {
+        setSide(width);
+    }
+
+    @Override
+    public void setLength(double length) {
+        setSide(length);
+    }
+
+    @Override
+    public String toString() {
+        return "A Square with side="
+                + getSide()
+                + ", which is a subclass of "
+                + super.toString();
+    }
+}
